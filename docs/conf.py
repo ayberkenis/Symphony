509a4f0da9423a73d74cdfd8e767d2171bc0e4fd
ayberkenis <ayberkenis@gmail.com>
2024-02-28 23:56:08 +0300

    update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../symphony/'))
+
 
 project = 'web-symphony'
 copyright = '2024, ayberkenis'
@@ -13,7 +17,11 @@ author = 'ayberkenis'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
